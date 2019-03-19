c18db1c480970b39749f88d7b04505eadf3a9f19
Edward <3zangelks@gmail.com>
2019-03-19 09:11:20 +0800

    chore(): add loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,26 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/index.tsx'),
+  entry: path.join(__dirname, 'src', 'index.tsx'),
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.ts', '.tsx', '.js', '.json']
+  },
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'index_bundle.js'
   },
-  plugins: [new HtmlWebpackPlugin()]
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html')
+    })
+  ]
 };
